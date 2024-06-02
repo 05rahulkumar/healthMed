c65fefda897b7fe03344912219b46570b0e912d8
05rahulkumar <rahulk160797@gmail.com>
2024-06-02 17:46:28 +0530

    home complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,16 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import {HttpClientModule } from '@angular/common/http';
-import { BoostrapModule } from './shared/boostrap/boostrap.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//packages
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ToastrModule } from 'ngx-toastr'; 
+
+//shared
+import { BoostrapModule } from './shared/boostrap/boostrap.module';
+import { MaterialModule } from './shared/material/material.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,8 +31,15 @@ import { BoostrapModule } from './shared/boostrap/boostrap.module';
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SlickCarouselModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     HttpClientModule,
-    BoostrapModule
+    BoostrapModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
